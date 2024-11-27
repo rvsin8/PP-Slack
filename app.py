4ee9e608ea4915dfa7e3377e3e8a4a96704bfc93
Rav S <rsingh0815@gmail.com>
2024-11-27 08:24:09 -0500

    Set up PostgreSQL database and roles & Successfully tested registration and login functionalities

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask # Core framework to define routes and handle HTTP requests
 from auth import auth_bp # Impoirt the blueprint from auth.py
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) # Creates instance of Flask class, __name__ tells Flask the location of the current python module.
+
+# Configure PostgreSQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rav:new_password@localhost/slack_clone'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize SQLAlchemy
+db = SQLAlchemy(app)
 
 # Register the auth blueprint
 app.register_blueprint(auth_bp)
@@ -11,4 +19,6 @@ def home():
     return 'Welcome to Slack Clone!'
 
 if __name__ == "__main__":  # Corrected "__main__" with double underscores, checks if the scrupt it being run directly.
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, host='0.0.0.0', port=3000)
