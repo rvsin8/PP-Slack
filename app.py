514e0499e2c90595c00572f4b2aca48a350e8ecc
Rav S <rsingh0815@gmail.com>
2024-11-27 06:47:06 -0500

    Initial commit, set up Flash app for Slack Clone with comments

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask #Core framework to define routes and handle HTTP requests
 
-app = Flask(__name__)
+app = Flask(__name__) #Creates instance of Flask class, __name__ tells Flask the location of the current python module.
 
-@app.route('/')
+@app.route('/') #Decorator that maps the root url to the home function
 def home():
     return 'Welcome to Slack Clone!'
 
-if __name__ == "__main__":  # Corrected "__main__" with double underscores
+if __name__ == "__main__":  # Corrected "__main__" with double underscores, checks if the scrupt it being run directly.
     app.run(debug=True, host='0.0.0.0', port=3000)
