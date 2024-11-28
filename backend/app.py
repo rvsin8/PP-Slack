41664f7bbc1197402e2f244dcfe6cab42a0a298e
Rav S <rsingh0815@gmail.com>
2024-11-27 21:12:46 -0500

    edits

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,16 @@
 from flask import Flask
-from backend.auth import auth_bp  # Import the blueprint from auth.py
-from flask_sqlalchemy import SQLAlchemy
-from backend.message import message_bp  # Import the message blueprint from message.py
-from backend.models import db  # Import db from models.py
-from backend.channel import channel_bp  # Import the channel blueprint from channel.py
-from backend.direct_message import direct_message_bp  # Import the direct message blueprint
+from flask_cors import CORS
 from flask_migrate import Migrate
-from auth import auth_bp  # Import the blueprint from auth.py
+from backend.auth import auth_bp
+from backend.message import message_bp
+from backend.models import db
+from backend.channel import channel_bp
+from backend.direct_message import direct_message_bp
 
 
 app = Flask(__name__)
+
+CORS(app)  # Allow all origins by default
 
 # Configure PostgreSQL database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rav:new_password@localhost/slack_clone'
@@ -27,7 +28,6 @@ app.register_blueprint(message_bp, url_prefix='/message')
 app.register_blueprint(channel_bp, url_prefix='/channel')  # Register the channel blueprint
 app.register_blueprint(direct_message_bp, url_prefix='/direct_message')
 
-
 @app.route('/')  # Decorator that maps the root URL to the home function
 def home():
     return 'Welcome to Slack Clone!'
@@ -35,4 +35,6 @@ def home():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Changed to 5000
+
+
